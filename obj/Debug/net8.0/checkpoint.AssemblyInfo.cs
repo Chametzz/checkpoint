720bb5fec7828a040fe2c4f554a52d8459078840
Cha <chamrlsmtz@gmail.com>
2025-03-05 21:29:43 -0600

    commit de prueba

--- obj/Debug/net8.0/checkpoint.AssemblyInfo.cs
+++ obj/Debug/net8.0/checkpoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("checkpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccf920b56369d0af7d8b594d66ce8ce00d7dbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("checkpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("checkpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
