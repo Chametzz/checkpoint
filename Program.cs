b101afffbc2ec8fe5637514a47c58d34b8aa4733
Wilmaxin <andy.max3106@gmail.com>
2025-03-08 14:39:20 -0600

    Cambios_Texto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿DB.SETDATABASE(Path.Combine(Directory.GetCurrentDirectory(), "database.db"));
+//Hola que tal
+//Viva la vida jajaja
+Console.WriteLine("Simon Bolivar");
 Window wind = new Window();
