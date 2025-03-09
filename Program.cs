c1ae0337ebd2e8c2f189704df0a9a10a706c9c3a
Wilmaxin <andy.max3106@gmail.com>
2025-03-08 18:19:10 -0600

    Ref

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ DB.SETDATABASE(Path.Combine(Directory.GetCurrentDirectory(), "database.db"));
 
 ModelEmployees Patata = new ModelEmployees();
 CheckPointUI layout = new CheckPointUI(new Window());
+Employee Empleado = null;
+
 
 Page Login = layout.CreatePage("login", (page) => {});
 
@@ -20,7 +22,25 @@ Login.SearchLabel<Form>("LOGINFORM").SetAction((form, data) => {
     form.SetWarning("FUNCIONA");
 });
 
-Page Home = layout.CreatePage("home", (page) => { });
+
+Page Home = layout.CreatePage("home", (page) => {
+    page.SetRef("welcome", $"Bienvenido {Empleado.Firstname}");
+    page.SetRef("id", $"ID:{Empleado.Id}");
+    page.SetRef("first_name", $"Nombre:{Empleado.Firstname}");
+    page.SetRef("last_name", $"Apellido:{Empleado.Lastname}");
+    page.SetRef("sex", $"Sexo:{Empleado.Sex}");
+    page.SetRef("birthdate", $"Fecha de Nacimiento:{Empleado.Birthdate}");
+    page.SetRef("phone_no", $"Telefono:{Empleado.Phoneno}");
+    page.SetRef("email", $"Correo Electronico:{Empleado.Email}");
+    page.SetRef("adress", $"Dirección:{Empleado.Adress}");
+    page.SetRef("hiredate", $"Fecha de Contratacion:{Empleado.Hiredate}");
+    page.SetRef("workdept", $"Departamento:{Empleado.Workdept}");
+    page.SetRef("job", $"Trabajo:{Empleado.Job}");
+    page.SetRef("salary", $"Salario:{Empleado.Salary}");
+});
+
+
+
 
 
 layout.wind.Execute();
