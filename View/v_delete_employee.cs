0468ae0bfe8b96b515f89ccd66c79136318428c5
Cha <chamrlsmtz@gmail.com>
2025-03-09 15:57:26 -0600

    se agrego el selector y se mejoró table

--- View/v_delete_employee.cs
+++
@@ -1,9 +0,0 @@
-
-public class DelEmpPage : Page
-{
-    public DelEmpPage(Window wind, string title, Action<Page>? toLoad = null) : base(wind, title, toLoad) {
-        Form delform = InsertLabel<Form>("¡ATENCIÓN, LOS DATOS NO PODRÁN RECUPERARSE!", ("name", "DELETEFORM"));
-        delform.InsertChild<Input>("Introduzca el id del empleado que desea eliminar:", ("name", "ID"), ("type", "number"), ("required", "true"));
-        delform.InsertChild<Input>("Eliminar", ("type", "submit"));
-    }
-}
