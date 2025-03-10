ea41d66cbb79a040ff26ca1b0b25c1cbceb57cea
Wilmaxin <andy.max3106@gmail.com>
2025-03-09 18:52:59 -0600

    SI

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using static System.Collections.Specialized.BitVector32;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 DB.SETDATABASE(Path.Combine(Directory.GetCurrentDirectory(), "database.db"));
 
@@ -12,7 +13,7 @@ ModelEmployees Patata = new ModelEmployees();
 CheckPointUI layout = new CheckPointUI(new Window());
 Employee Empleado = null;
 Page Home = null;
-
+Page Registro = null;
 
 Page Login = layout.CreatePage("login", (page) => {});
 
@@ -51,5 +52,24 @@ Home = layout.CreatePage("home", (page) => {
         page.wind?.ReplacePage(Login);
 
     };
+
 });
+
+Registro = layout.CreatePage("register employee", (page) => {
+    page.SearchLabel<Form>("REGISTERFORM").SetAction((form, data) => {
+        Empleado = Patata.RegisterEmployee(data["FIRTSNAME"]);
+    });
+
+
+
+    });
+
+    
+
+
+
+Dictionary<string, List<int>> diccionario = new Dictionary<string, List<int>>();
+
+
+
 layout.wind.Execute();
