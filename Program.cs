7f77268d1a021a2f15b8591f6bd2749cf1ec7e93
Cha <chamrlsmtz@gmail.com>
2025-03-30 17:17:57 -0600

    cambio que no me acuerdo

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 DB.SETDATABASE(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "database.db"));
 
+int screenWidth = Console.WindowWidth;
+int screenHeight = Console.WindowHeight;
+
+Thread resize = new Thread(() => {
+    while(true) {
+        if(Console.WindowWidth != screenWidth || Console.WindowHeight != screenHeight) {
+            screenWidth = Console.WindowWidth;
+            screenHeight = Console.WindowHeight;
+            Controller.layout.wind.RefreshPage();
+        }
+        Thread.Sleep(1000/60);
+    }
+});
+resize.Start();
+
 C_Employees employees = new C_Employees();
 C_Games games = new C_Games();
 C_Prizes prizes = new C_Prizes();
